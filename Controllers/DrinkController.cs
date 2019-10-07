a3a2ef0ee6abcc5c4482dd907180e9742f529e0b
Stephanie Hooker <stephanieh799@gmail.com>
2019-10-06 20:41:09 -0600

    drinks are loading to console

--- Controllers/DrinkController.cs
+++ Controllers/DrinkController.cs
@@ -7,16 +7,43 @@ namespace csharp_burgershack.Controllers
   {
     private DrinkService _drinkService { get; set; } = new DrinkService();
 
+
     public void UserInput()
     {
-      Console.WriteLine("press Q to quit");
+      _drinkService.GetDrinks();
+      Print();
       string choice = Console.ReadLine().ToLower();
+      Console.Clear();
       switch (choice)
       {
         case "q":
           Environment.Exit(1);
           break;
+        default:
+          if (int.TryParse(choice, out int index))
+          {
+            //print out drink details
+            _drinkService.GetDrinks(index - 1);
+            Print();
+            Console.ReadKey();
+            Console.Clear();
+          }
+          else
+          {
+            Console.WriteLine("invalid command");
+          }
+          break;
       }
+
+    }
+    private void Print()
+    {
+      foreach (string message in _drinkService.Messages)
+      {
+        Console.WriteLine(message);
+      }
+      _drinkService.Messages.Clear();
+      Console.WriteLine("Type a number to see details or Q to quit");
     }
   }
 }
--- Services/DrinkService.cs
+++ Services/DrinkService.cs
@@ -6,32 +6,51 @@ namespace csharp_burgershack.Services
   public class DrinkService
   {
     public List<Drink> Drinks { get; private set; }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public List<string> Messages { get; set; }
     public DrinkService() //constructor
     {
       Drinks = new List<Drink>();
+      Messages = new List<string>();
+      Setup();
     }
+
+
+    public void Setup()
+    {
+      Drink coffee = new Drink("Carmel Mocha", 5, "Hot and Yummy");
+      Drink tea = new Drink("Milk Tea", 4, "yummy");
+      Drink water = new Drink("water", 2, "it's good for you");
+      Drink lacroix = new Drink("LaCroix", 3, "lightly flavored water");
+
+      Drinks.AddRange(new Drink[] { coffee, tea, water, lacroix });
+    }
+
+    public void GetDrinks(int index)
+    {
+      if (index < Drinks.Count && index > -1)
+      {
+        Drink d = Drinks[index];
+        Messages.Add($@"
+        Title: {d.Title}
+        Price: {d.Price}
+        Description: {d.Description}
+        press any key to return to the main menu
+        ");
+      }
+      else
+      {
+        Messages.Add("Invalid Choice");
+      }
+    }
+    public void GetDrinks()
+    {
+      Messages.Add("Available Drinks");
+      for (int i = 0; i < Drinks.Count; i++)
+      {
+        Drink d = Drinks[i];
+        Messages.Add($"{i + 1}: {d.Title} - {d.Price} - {d.Description}");
+      }
+    }
+
   }
 }
